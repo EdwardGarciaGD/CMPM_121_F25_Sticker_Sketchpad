4557b603263b53f2229ca71e3c876419d34d093a
EdwardGarciaGD <egarc216@ucsc.edu>
2025-10-16 07:07:35 +0000

    Header And Canvas Created With Document.CreateElement, Canvas Border Created In Style.css (Step 1 Complete)
    -Added a circle to test canvas display

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+//document.body.innerHTML = ``;
+
+const heading = document.createElement("h1");
+heading.textContent = "Sketch On Me";
+document.body.appendChild(heading);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "sketchCanvas";
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
+// Testing canvas
+if (ctx) {
+  ctx.fillStyle = "#85f4eeff";
+  ctx.beginPath();
+  ctx.arc(128, 128, 64, 0, Math.PI * 2);
+  ctx.fill();
+}
