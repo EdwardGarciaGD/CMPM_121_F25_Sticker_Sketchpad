3689243e90a9dc14b869f17abee0f057bb6da511
EdwardGarciaGD <egarc216@ucsc.edu>
2025-10-17 22:42:54 +0000

    Cleaned Code, Added Bus Event Target For Redraw Function That Calls After "drawing-changed" With Arrays (Step 3 Complete)
    -Added array for current line being drawn
    -Added array for all lines drawn
    -Added bus event that finds "drawing-changed" event with notify function
    -Added redraw that iterates lines array to display on canvas

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import "./style.css";
 
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d")!;
+const lines: { x: number; y: number }[][] = [];
+const bus = new EventTarget();
 const cursor = { active: false, x: 0, y: 0 };
+
+let currentLine: { x: number; y: number }[] = [];
 
 document.body.appendChild(createDocuElement("h1", "Sketch On Me"));
 
@@ -17,33 +21,58 @@ const clearButton = createDocuElement("button", "Clear Drawing");
 clearButton.classList.add("clear-button");
 document.body.appendChild(clearButton);
 
+bus.addEventListener("drawing-changed", redraw);
+
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+  lines.push(currentLine);
+  currentLine.push({ x: cursor.x, y: cursor.y });
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.active) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    currentLine.push({ x: cursor.x, y: cursor.y });
+    notify("drawing-changed");
   }
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
+  currentLine = [];
+  notify("drawing-changed");
 });
 
 clearButton.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines.splice(0, lines.length);
+  notify("drawing-changed");
 });
 
 function createDocuElement(tag: string, content: string) {
   const element = document.createElement(tag);
   element.textContent = content;
   return element;
+}
+
+function notify(name: string) {
+  bus.dispatchEvent(new Event(name));
+}
+
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (const line of lines) {
+    if (line.length > 1) {
+      ctx.beginPath();
+      const { x, y } = line[0];
+      ctx.moveTo(x, y);
+      for (const { x, y } of line) {
+        ctx.lineTo(x, y);
+      }
+      ctx.stroke();
+    }
+  }
 }
